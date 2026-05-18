f814f6823caf949ab3a0fba1b77c0114208f72ad
Juuso <jv0832@edu.turku.fi>
2026-05-18 15:05:34 +0300

    tehtävä 22

--- portfolio/keskustelusivu.js
+++ portfolio/keskustelusivu.js
@@ -1,1 +1,23 @@
-console.log("Keskustelu-sivu avattu");
+console.log("Keskustelusivu avattu");
+
+function send(e) {
+    e.preventDefault();
+
+    let form = e.target.form;
+
+    let birthday = form.birthday.value;
+    let email = form.email.value;
+    let type = form.type.value;
+    let usage = form.usage.value;
+    let body = form.body.value;
+    let marketing = form.marketing.checked;
+
+    console.log("Syntymäpäivä:", birthday);
+    console.log("Sähköposti:", email);
+    console.log("Tyyppi:", type);
+    console.log("Käyttötarkoitus:", usage);
+    console.log("Viesti:", body);
+    console.log("Markkinointilupa:", marketing);
+
+    document.getElementById("kommenttinappi").close();
+}
