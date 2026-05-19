9a0de605581e23a0f991b31c8430990fd0c09066
Juuso <jv0832@edu.turku.fi>
2026-05-19 21:48:27 +0300

    tehtävä 29

--- portfolio/Muuttujia ja tietotyyppejä.js
+++ portfolio/Muuttujia ja tietotyyppejä.js
@@ -5,6 +5,8 @@ let b = 5;
 let c = 7;
 let d = 15;
 
+// Peruslaskut
+
 function summa(a, b) {
     alert(Number(a) + Number(b));
 }
@@ -21,29 +23,88 @@ function jako(a, b) {
     alert(Number(a) / Number(b));
 }
 
+// Laskin
+
 function laskin_summa() {
+
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
+
     summa(eka, toka);
 }
 
 function laskin_vahennys() {
+
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
+
     vahennys(eka, toka);
 }
 
 function laskin_kerto() {
+
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
+
     kerto(eka, toka);
 }
 
 function laskin_jako() {
+
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
+
     jako(eka, toka);
 }
+
+// Math-toiminnot
+
+function laskin_abs() {
+
+    let eka = document.getElementById("numero1").value;
+
+    alert(Math.abs(Number(eka)));
+}
+
+function laskin_sqrt() {
+
+    let eka = document.getElementById("numero1").value;
+
+    alert(Math.sqrt(Number(eka)));
+}
+
+function laskin_pow() {
+
+    let eka = document.getElementById("numero1").value;
+    let toka = document.getElementById("numero2").value;
+
+    alert(Math.pow(Number(eka), Number(toka)));
+}
+
+function laskin_max() {
+
+    let eka = document.getElementById("numero1").value;
+    let toka = document.getElementById("numero2").value;
+
+    alert(Math.max(Number(eka), Number(toka)));
+}
+
+function laskin_min() {
+
+    let eka = document.getElementById("numero1").value;
+    let toka = document.getElementById("numero2").value;
+
+    alert(Math.min(Number(eka), Number(toka)));
+}
+
+function laskin_round() {
+
+    let eka = document.getElementById("numero1").value;
+
+    alert(Math.round(Number(eka)));
+}
+
+// Päivämäärä ja aika
 
 function naytaAika() {
 
@@ -68,20 +129,42 @@ function naytaAika() {
     document.getElementById("kuukausi").innerHTML = kuukausi;
 }
 
-let veijo = new Set(["luku", "poisto"]);
-let elvira = new Set(["luku", "kirjoitus", "muokkaus"]);
-let mehdi = new Set(["luku", "muokkaus", "poisto"]);
-let tuuli = new Set(["kirjoitus", "poisto"]);
+// Setit
+
+let veijo = new Set([
+    "luku",
+    "poisto"
+]);
+
+let elvira = new Set([
+    "luku",
+    "kirjoitus",
+    "muokkaus"
+]);
+
+let mehdi = new Set([
+    "luku",
+    "muokkaus",
+    "poisto"
+]);
+
+let tuuli = new Set([
+    "kirjoitus",
+    "poisto"
+]);
 
 function tulostaKayttaja(nimi, setti) {
 
     let ul = document.querySelector(`#${nimi} ul`);
+
     ul.innerHTML = "";
 
     for (let oikeus of setti) {
 
         let li = document.createElement("li");
+
         li.innerHTML = oikeus;
+
         ul.appendChild(li);
     }
 }
@@ -91,24 +174,36 @@ tulostaKayttaja("elvira", elvira);
 tulostaKayttaja("mehdi", mehdi);
 tulostaKayttaja("tuuli", tuuli);
 
-let veijotuuli = new Set([...veijo, ...tuuli]);
+// Yhdistelmät
 
-let mehdielvira = new Set([...mehdi].filter(x => elvira.has(x)));
+let veijotuuli = new Set([
+    ...veijo,
+    ...tuuli
+]);
+
+let mehdielvira = new Set(
+    [...mehdi].filter(x => elvira.has(x))
+);
 
 let elviratuuli = new Set(
     [...elvira].filter(x => !tuuli.has(x))
-    .concat([...tuuli].filter(x => !elvira.has(x)))
+    .concat(
+        [...tuuli].filter(x => !elvira.has(x))
+    )
 );
 
 function tulostaSetti(id, setti) {
 
     let ul = document.getElementById(id);
+
     ul.innerHTML = "";
 
     for (let arvo of setti) {
 
         let li = document.createElement("li");
+
         li.innerHTML = arvo;
+
         ul.appendChild(li);
     }
 }
