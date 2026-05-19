d60c057b56363a73d32c1379e2bbc1d7c2b2cf11
Juuso <jv0832@edu.turku.fi>
2026-05-19 18:28:07 +0300

    tehtävä 27

--- portfolio/Muuttujia ja tietotyyppejä.js
+++ portfolio/Muuttujia ja tietotyyppejä.js
@@ -1,10 +1,12 @@
 console.log("Muuttujia ja tietotyyppejä-sivu avattu");
 
+// muuttujat
 let a = 3;
 let b = 5;
 let c = 7;
 let d = 15;
 
+// peruslaskut
 function summa(a, b) {
     alert(Number(a) + Number(b));
 }
@@ -21,30 +23,50 @@ function jako(a, b) {
     alert(Number(a) / Number(b));
 }
 
+// laskin (input-kentät)
 function laskin_summa() {
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
-
     summa(eka, toka);
 }
 
 function laskin_vahennys() {
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
-
     vahennys(eka, toka);
 }
 
 function laskin_kerto() {
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
-
     kerto(eka, toka);
 }
 
 function laskin_jako() {
     let eka = document.getElementById("numero1").value;
     let toka = document.getElementById("numero2").value;
-
     jako(eka, toka);
+}
+
+function naytaAika() {
+
+    let nyt = new Date();
+
+    let paivamaara = nyt.toLocaleDateString("fi-FI");
+    let aika = nyt.toLocaleTimeString("fi-FI");
+    let paivaJaAika = nyt.toLocaleString("fi-FI");
+
+    let viikonpaiva = nyt.toLocaleDateString("fi-FI", {
+        weekday: "long"
+    });
+
+    let kuukausi = nyt.toLocaleDateString("fi-FI", {
+        month: "long"
+    });
+
+    document.getElementById("paivamaara").innerHTML = paivamaara;
+    document.getElementById("aika").innerHTML = aika;
+    document.getElementById("paivaJaAika").innerHTML = paivaJaAika;
+    document.getElementById("viikonpaiva").innerHTML = viikonpaiva;
+    document.getElementById("kuukausi").innerHTML = kuukausi;
 }
